46e353b605c6ec0c729aff47471e97ea2ea8eeef
joannelee1990 <33495443+joannelee1990@users.noreply.github.com>
2018-06-22 18:14:52 -0500

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [
+    {"name":"Michael",
+        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=0zAtW9GeCc_J5gKRha7wCw&q=fries&oq=fries&gs_l=img.3..0i67k1j0l4j0i67k1l3j0j0i67k1.104435.104958.0.105167.5.5.0.0.0.0.106.398.2j2.4.0....0...1c.1.64.img..1.4.397....0.2zedwDPrW8w",
+        "scores":[5,1,4,4,5,1,2,5,4,1]},
+    {"name":"Joanne",
+        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=PjEtW86IAs-45gLbiqOAAQ&q=pasta&oq=pasta&gs_l=img.3..0i67k1j0l3j0i67k1j0l2j0i67k1j0l2.12016.12447.0.12654.5.5.0.0.0.0.177.683.1j4.5.0....0...1c.1.64.img..0.5.680....0.Nsof_RQ2rMU",
+        "scores":[5,5,5,5,5,5,5,5,5,5]},
+    {"name":"Jenny",
+        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=TDEtW9jbDKjX5gK84KawDw&q=fried+rice&oq=fried+rice&gs_l=img.3..0l10.11688.12986.0.13102.10.8.0.2.2.0.129.787.5j3.8.0....0...1c.1.64.img..0.10.806...0i67k1.0.zgHl9BTJ_Pg",
+        "scores":[4,1,1,3,5,3,3,3,2,4]},
+    {"name":"Ken",
+        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=WjEtW9HjKNGG5wKokqOoBQ&q=fried+chicken&oq=fried+chicken&gs_l=img.3..0l10.13442.14994.0.15245.13.10.0.3.3.0.170.1062.5j5.10.0....0...1c.1.64.img..1.12.924...0i67k1.0.AyT2cRZDFUY",
+        "scores":[1,3,2,4,4,2,1,2,3,2]},
+    {"name":"Alex",
+        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=ajEtW_KzOavW5gL1vL-QAw&q=ice+cream&oq=ice+cream&gs_l=img.3..0i67k1l2j0j0i67k1j0l2j0i67k1j0l3.9779.10713.0.10961.9.6.0.3.3.0.130.663.2j4.6.0....0...1c.1.64.img..1.8.578....0.Fa8F-mRfmxE",
+        "scores":[1,1,1,1,1,1,1,1,1,1]},
+
+  ];
