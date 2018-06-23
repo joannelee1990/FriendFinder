58f698458218d9a7bddd5adf43242ae47afbff20
joannelee1990 <33495443+joannelee1990@users.noreply.github.com>
2018-06-22 23:45:06 -0500

    second push

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,19 @@
 var friendsArray = [
     {"name":"Michael",
-        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=0zAtW9GeCc_J5gKRha7wCw&q=fries&oq=fries&gs_l=img.3..0i67k1j0l4j0i67k1l3j0j0i67k1.104435.104958.0.105167.5.5.0.0.0.0.106.398.2j2.4.0....0...1c.1.64.img..1.4.397....0.2zedwDPrW8w",
+        "photo":"https://cms.splendidtable.org/sites/default/files/styles/w2000/public/french-fries.jpg?itok=FS-YwUYH",
         "scores":[5,1,4,4,5,1,2,5,4,1]},
     {"name":"Joanne",
-        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=PjEtW86IAs-45gLbiqOAAQ&q=pasta&oq=pasta&gs_l=img.3..0i67k1j0l3j0i67k1j0l2j0i67k1j0l2.12016.12447.0.12654.5.5.0.0.0.0.177.683.1j4.5.0....0...1c.1.64.img..0.5.680....0.Nsof_RQ2rMU",
+        "photo":"https://assets.bonappetit.com/photos/5aa9665c275dc52331d9184b/16:9/w_1200,c_limit/pantry-pasta.jpg",
         "scores":[5,5,5,5,5,5,5,5,5,5]},
     {"name":"Jenny",
-        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=TDEtW9jbDKjX5gK84KawDw&q=fried+rice&oq=fried+rice&gs_l=img.3..0l10.11688.12986.0.13102.10.8.0.2.2.0.129.787.5j3.8.0....0...1c.1.64.img..0.10.806...0i67k1.0.zgHl9BTJ_Pg",
+        "photo":"https://cdn.cpnscdn.com/static.coupons.com/ext/kitchme/images/recipes/600x400/low-carb-southern-fried-chicken-recipe_16091.jpg",
         "scores":[4,1,1,3,5,3,3,3,2,4]},
     {"name":"Ken",
-        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=WjEtW9HjKNGG5wKokqOoBQ&q=fried+chicken&oq=fried+chicken&gs_l=img.3..0l10.13442.14994.0.15245.13.10.0.3.3.0.170.1062.5j5.10.0....0...1c.1.64.img..1.12.924...0i67k1.0.AyT2cRZDFUY",
+        "photo":"https://www.chewboom.com/wp-content/uploads/2018/01/50-Percent-Off-Online-Pizza-Orders-At-Pizza-Hut-Through-January-8-2018-678x381.jpg",
         "scores":[1,3,2,4,4,2,1,2,3,2]},
     {"name":"Alex",
-        "photo":"https://www.google.com/search?biw=1474&bih=799&tbm=isch&sa=1&ei=ajEtW_KzOavW5gL1vL-QAw&q=ice+cream&oq=ice+cream&gs_l=img.3..0i67k1l2j0j0i67k1j0l2j0i67k1j0l3.9779.10713.0.10961.9.6.0.3.3.0.130.663.2j4.6.0....0...1c.1.64.img..1.8.578....0.Fa8F-mRfmxE",
+        "photo":"https://hips.hearstapps.com/del.h-cdn.co/assets/15/18/4000x2000/landscape-1430425441-alabama-jimnnicks-bbqnachos.jpg?resize=768:*",
         "scores":[1,1,1,1,1,1,1,1,1,1]},
-
   ];
+
+  module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 var path = require("path");
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
@@ -13,20 +13,25 @@ module.exports = function(app) {
     var bestMatch =0; 
     var scoresArray =[];
     var userInput = req.body;
+
     console.log("this is the req.body: " + req.body);
-    var userresponse = userInput.scores
+    var userresponse = userInput.scores;
+    // var me = JSON.stringify(userInput);
+    console.log(userInput.scores);
+    console.log(userresponse);
+    // console.log(typeof(req.body.score[0]));
+
     console.log("this is the userresponse: " + userresponse);
 
     var matchName="";
     var matchImage="";
-    for (var i =0 ; i< friends.length; i++){
+    for (var i =0 ; i <friends.length; i++){
         var diff = 0;
 
         for (var j=0; j<userresponse.length; j++){
             diff += Math.abs(parseInt(friends[i].scores[j]- userresponse[j]))
         }
-       scoresArray.push(diff)
-
+       scoresArray.push(diff);
     }
 
     for(var i =0 ; i<scoresArray.length; i++){
@@ -36,11 +41,14 @@ module.exports = function(app) {
     }
     
     var bestfriend = friends[bestMatch];
+    console.log("b",bestfriend);
+    console.log("c",bestMatch);
+
     res.json(bestfriend);
     console.log(bestfriend);
 
-
-    friends.push(req.body);
+    console.log(typeof(friends));
+    // friends.push(req.body);
   });
 };
 
